19d5cdf149be11f0e770cf243d8fe34736047917
ronaldoapf <ronaldo.alves.1997@gmail.com>
2024-08-05 12:07:01 -0300

    chore: add shadcn and theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <h1>Layout</h1>
-        {children}
-      </body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1 className="mb-1 text-red-500">Teste</h1>
+  return <Button>Teste</Button>
 }
